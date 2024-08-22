3c7b76a27037d554bd6b6618ab69d4dfa7b9cb1c
0BooM <149960425+0BooM@users.noreply.github.com>
2024-08-22 15:10:23 +0200

    Update: Now the DOM is properly managed and changed when searching for a new location to show weather

--- src/modules/DisplayController.js
+++ src/modules/DisplayController.js
@@ -1,6 +1,50 @@
 import Api from './Api';
+import fogIcon from '../icons/fog.svg';
 
 const DisplayController = (() => {
+  function getWeatherIcon(iconName) {
+    try {
+      // Dynamiczne importowanie ikony
+      return import(`../icons/${iconName}.svg`).then(
+        (module) => module.default
+      );
+    } catch (error) {
+      console.error(`Nie można załadować ikony: ${iconName}`, error);
+      return ''; // Zwróć pusty string lub ścieżkę do domyślnej ikony w przypadku błędu
+    }
+  }
+
+  function updateWeatherIcon(iconName) {
+    getWeatherIcon(iconName).then((iconSrc) => {
+      const weatherIcon = document.getElementById('weather-icon');
+      weatherIcon.src = iconSrc;
+    });
+  }
+
+  function renderMainWeatherInfo(data) {
+    const location = document.getElementById('location');
+    const temperature = document.getElementById('temperature');
+    const unit = document.getElementById('unit'); // Osobny element dla jednostki
+    const day = document.getElementById('day');
+    const hour = document.getElementById('hour');
+    const cloudStatus = document.getElementById('cloud-status');
+    const precipPercent = document.getElementById('precip-percent');
+    const humidityPercent = document.getElementById('humidity-percent');
+    const windSpeed = document.getElementById('wind-speed');
+
+    location.textContent = data.address;
+    temperature.firstChild.nodeValue = data.temp;
+    unit.innerHTML = '℃';
+    day.textContent = data.day;
+    hour.textContent = data.hour;
+    cloudStatus.textContent = data.conditions;
+    precipPercent.textContent = `${data.precipprob}%`;
+    humidityPercent.textContent = `${data.humidity}%`;
+    windSpeed.textContent = `${data.windspeed} km/h`;
+
+    updateWeatherIcon(data.icon);
+  }
+
   function getUserLocationInput() {
     const input = document.querySelector('#userLocation');
     input.addEventListener('keypress', async (e) => {
@@ -8,6 +52,7 @@ const DisplayController = (() => {
         const data = await Api.getWeatherData(input.value);
         console.log(data);
         input.value = '';
+        renderMainWeatherInfo(data);
       }
     });
   }
